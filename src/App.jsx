e54402b060c08b0c9e98c40393b2acaf54e2d0d3
VCarames <109115310+VCarames@users.noreply.github.com>
2024-08-23 10:39:57 -0700

    App: Setup Router With Routes For Main Pages
    This code sets up the main routing structure using “react-router-dom”. It creates routes for the “Home”, “Category”, “Product”, and “Checkout” pages, with an “Error” page as a fallback for any errors. The “App” component returns a “RouterProvider” to manage the navigation between these pages.

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Error from "./pages/Error";
+import Home from "./pages/Home";
+import Category from "./pages/Category";
+import Product from "./pages/Product";
+import Checkout from "./pages/Checkout";
+
+const router = createBrowserRouter([
+  {
+    errorElement: <Error />,
+
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+
+      {
+        path: "/category",
+        element: <Category />,
+      },
+
+      {
+        path: "/product",
+        element: <Product />,
+      },
+
+      {
+        path: "/checkout",
+        element: <Checkout />,
+      },
+    ],
+  },
+]);
+
+function App() {
+  return <RouterProvider router={router} />;
+}
+
+export default App;
