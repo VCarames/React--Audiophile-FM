ff2ef951e08e2d6ed71e14d904862b64ab8a2fca
VCarames <109115310+VCarames@users.noreply.github.com>
2024-08-31 22:00:31 -0700

    ProductCategory: AboutSection Added
    The updated "ProductCategory" component now includes the "AboutSection" component.

--- src/pages/ProductCategory.jsx
+++ src/pages/ProductCategory.jsx
@@ -1,9 +1,11 @@
 import ProductCategorySection from "../components/products/ProductCategorySection";
+import AboutSection from "../components/about/AboutSection";
 
 function ProductCategory() {
   return (
     <main>
       <ProductCategorySection />
+      <AboutSection />
     </main>
   );
 }
