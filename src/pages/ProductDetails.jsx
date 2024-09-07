d8af046b388483a448fa4b10d9d26666c8d77ab5
VCarames <109115310+VCarames@users.noreply.github.com>
2024-09-07 08:37:27 -0700

    ProductDetails Update: ProductDetailsSection Added to Component
    This component renders the "ProductDetailsSection" inside a <main> element, serving as a wrapper for displaying detailed product information.

---
+++ src/pages/ProductDetails.jsx
@@ -0,0 +1,11 @@
+import ProductDetailsSection from "../productDetails/ProductDetailsSection";
+
+function ProductDetails() {
+  return (
+    <main>
+      <ProductDetailsSection />
+    </main>
+  );
+}
+
+export default ProductDetails;
