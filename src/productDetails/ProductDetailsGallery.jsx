d55bdc9321a69696497dc6406971a62778e9b623
VCarames <109115310+VCarames@users.noreply.github.com>
2024-09-06 23:56:59 -0700

    ProductDetailsGallery Update: Updated to Handle Responsive Image Sources
    The updated code restructures the gallery to handle responsive images for "mobile", "tablet", and "desktop" views. It now maps over the mobile images and uses desktop and tablet arrays for responsive media queries. Each picture gets a unique class name based on its index for more specific styling.

--- src/productDetails/ProductDetailsGallery.jsx
+++ src/productDetails/ProductDetailsGallery.jsx
@@ -1,14 +1,20 @@
 const ProductDetailsGallery = ({ gallery }) => {
+  const { mobile, tablet, desktop } = gallery;
+
   return (
     <div className="product-details__gallery">
-      <h2 className="product-details__gallery-title">Gallery</h2>
-      {gallery.map((image, index) => (
-        <picture className="product-details__gallery-picture" key={index}>
-          <source media="(min-width: 62.5rem)" srcSet={image.large} />
-          <source media="(min-width: 47.5rem)" srcSet={image.medium} />
+      {mobile.map((image, index) => (
+        <picture
+          className={`product-details__gallery-picture product-details__gallery-picture--${
+            index + 1
+          }`}
+          key={index}
+        >
+          <source media="(min-width: 62.5rem)" srcSet={desktop[index].image} />
+          <source media="(min-width: 47.5rem)" srcSet={tablet[index].image} />
           <img
-            src={image.small}
-            alt={image.alt}
+            src={mobile[index].image}
+            alt={mobile[index].alt}
             className="product-details__gallery-picture--img"
           />
         </picture>
