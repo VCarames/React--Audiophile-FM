c65a4477abde80566b95bb63db1a2915ef430fbd
VCarames <109115310+VCarames@users.noreply.github.com>
2024-09-07 00:02:41 -0700

    CartSlice Created: Manages Cart State
    This Redux slice handles cart operations including adding, deleting, increasing, and decreasing item quantities, and clearing the cart. It includes actions for updating item quantities and recalculating total prices. Selectors are provided to get the cart contents, total quantity, total price, and quantity of an item by its ID.

---
+++ src/components/cart/CartSlice.js
@@ -0,0 +1,69 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  cart: [],
+};
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState,
+  reducers: {
+    addItem(state, action) {
+      const existingItem = state.cart.find(
+        (item) => item.productId === action.payload.productId
+      );
+      if (existingItem) {
+        existingItem.quantity++;
+        existingItem.totalPrice =
+          existingItem.quantity * existingItem.currentPrice;
+      } else {
+        state.cart.push(action.payload);
+      }
+    },
+    deleteItem(state, action) {
+      state.cart = state.cart.filter(
+        (item) => item.productId !== action.payload
+      );
+    },
+    increaseItemQuantity(state, action) {
+      const item = state.cart.find((item) => item.productId === action.payload);
+      if (item) {
+        item.quantity++;
+        item.totalPrice = item.quantity * item.currentPrice;
+      }
+    },
+    decreaseItemQuantity(state, action) {
+      const item = state.cart.find((item) => item.productId === action.payload);
+      if (item) {
+        item.quantity--;
+        item.totalPrice = item.quantity * item.currentPrice;
+        if (item.quantity === 0)
+          cartSlice.caseReducers.deleteItem(state, action);
+      }
+    },
+    clearCart(state) {
+      state.cart = [];
+    },
+  },
+});
+
+export const {
+  addItem,
+  deleteItem,
+  increaseItemQuantity,
+  decreaseItemQuantity,
+  clearCart,
+} = cartSlice.actions;
+
+export default cartSlice.reducer;
+
+export const getCart = (state) => state.cart.cart;
+
+export const getTotalCartQuantity = (state) =>
+  state.cart.cart.reduce((sum, item) => sum + item.quantity, 0);
+
+export const getTotalCartPrice = (state) =>
+  state.cart.cart.reduce((sum, item) => sum + item.totalPrice, 0);
+
+export const getCurrentQuantityById = (id) => (state) =>
+  state.cart.cart.find((item) => item.productId === id)?.quantity ?? 1;
