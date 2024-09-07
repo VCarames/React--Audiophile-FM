d3eabe64e30779bc617d328e5a81b945da9e2a64
VCarames <109115310+VCarames@users.noreply.github.com>
2024-09-06 23:50:07 -0700

    ProductDetailsContainer Update: Redux Integration and Image Optimization
    The updated "ProductDetailsContainer" uses Redux to get the current product quantity and includes a quantity control with "UpdateItemQuantity". It optimizes images for desktop, tablet, and mobile, and groups quantity control and "AddToCartButton" for easy interaction.

--- src/productDetails/ProductDetailsContainer.jsx
+++ src/productDetails/ProductDetailsContainer.jsx
@@ -1,28 +1,39 @@
+import { useSelector } from "react-redux";
 import AddToCartButton from "../components/buttons/AddToCartButton";
+import UpdateItemQuantity from "../components/buttons/UpdateItemQuantity";
 import { formatCurrency } from "/src/utilities/formatCurrency.js";
+import { getCurrentQuantityById } from "../components/cart/CartSlice";
 
 const ProductDetailsContainer = ({ product }) => {
-  const { ribbon, showRibbon, name, description, price, images } = product;
+  const { id, ribbon, showRibbon, name, description, price, productImages } =
+    product;
+
+  const currentQuantity = useSelector(getCurrentQuantityById(id));
 
   return (
     <div className="product-details__container">
-      <div className="product-details__info">
-        {showRibbon && <div className="product-details__ribbon">{ribbon}</div>}
-        <h1 className="product-details__name">{name}</h1>
-        <p className="product-details__description">{description}</p>
-        <p className="product-details__price">{formatCurrency(price)}</p>
-        <AddToCartButton product={product} />
-      </div>
-
       <picture className="product-details__picture">
-        <source media="(min-width: 62.5rem)" srcSet={images.large} />
-        <source media="(min-width: 47.5rem)" srcSet={images.medium} />
+        <source media="(min-width: 62.5rem)" srcSet={productImages.desktop} />
+        <source media="(min-width: 47.5rem)" srcSet={productImages.tablet} />
         <img
-          src={images.small}
+          src={productImages.mobile}
           alt={name}
           className="product-details__picture--img"
         />
       </picture>
+      <div className="product-details__info">
+        {showRibbon && <p className="product-details__ribbon">{ribbon}</p>}
+        <h1 className="product-details__name">{name}</h1>
+        <p className="product-details__description">{description}</p>
+        <p className="product-details__price">{formatCurrency(price)}</p>
+        <div className="product-details__controls">
+          <UpdateItemQuantity
+            productId={id}
+            currentQuantity={currentQuantity}
+          />
+          <AddToCartButton product={product} />
+        </div>
+      </div>
     </div>
   );
 };
