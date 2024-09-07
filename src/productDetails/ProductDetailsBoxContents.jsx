ab276d0de6f1c98492f1009f94d14bd8bd16959e
VCarames <109115310+VCarames@users.noreply.github.com>
2024-09-06 23:54:54 -0700

    ProductDetailsBoxContents Updated: Changed List to Ordered & Updated Class Names
    The updated code switches from an unordered list (ul) to an ordered list (ol), adds more specific class names ("box-contents--quantity" and "box-contents--item"), and enhances readability by splitting the quantity and item into separate spans.

--- src/productDetails/ProductDetailsBoxContents.jsx
+++ src/productDetails/ProductDetailsBoxContents.jsx
@@ -2,16 +2,18 @@ const ProductDetailsBoxContents = ({ boxContents }) => {
   return (
     <div className="product-details__box-contents">
       <h2 className="product-details__box-contents-title">In the Box</h2>
-      <ul className="product-details__box-contents-list">
+      <ol className="product-details__box-contents-list">
         {boxContents.map((content, index) => (
           <li className="product-details__box-contents-item" key={index}>
-            <span className="product-details__box-contents-quantity">
+            <span className="product-details__box-contents--quantity">
               {content.quantity}
             </span>
-            {content.item}
+            <span className="product-details__box-contents--item">
+              {content.item}
+            </span>
           </li>
         ))}
-      </ul>
+      </ol>
     </div>
   );
 };
