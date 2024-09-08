767c43844febd4e32fa4ccbcaf2ff801f568358e
VCarames <109115310+VCarames@users.noreply.github.com>
2024-09-07 21:08:17 -0700

    AboutText: New Component for Displaying Text Content in the "About Us" Section
    The "AboutText" component presents the textual information for the "About Us" section. It includes a heading and a descriptive paragraph.

---
+++ src/components/about/AboutText.jsx
@@ -0,0 +1,20 @@
+function AboutText() {
+  return (
+    <div className="about-text">
+      <h3 className="about-text__heading">
+        Bringing you the
+        <span className="about-text__heading--highlight"> best</span> audio gear
+      </h3>
+      <p className="about-text__description">
+        Located at the heart of New York City, Audiophile is the premier store
+        for high-end headphones, earphones, speakers, and audio accessories. We
+        have a large showroom and luxury demonstration rooms available for you
+        to browse and experience a wide range of our products. Stop by our store
+        to meet some of the fantastic people who make Audiophile the best place
+        to buy your portable audio equipment.
+      </p>
+    </div>
+  );
+}
+
+export default AboutText;
