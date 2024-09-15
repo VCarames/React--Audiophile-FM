3d097b63bfaa42450350ca558e619c70127f59b3
VCarames <109115310+VCarames@users.noreply.github.com>
2024-09-14 20:51:27 -0700

    Home Page: Added ProductFeaturedSection
    The updated code adds the "ProductFeaturedSection" component to the "Home" page.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import Hero from "../components/intros/Hero";
 import CategoryLinkSection from "../components/categoryLinks/CategoryLinkSection";
+import ProductFeaturedSection from "../productFeatured/ProductFeaturedSection";
 
 function Home() {
   return (
     <>
       <Hero />
       <CategoryLinkSection categoryLinkClass="category--home" />
+      <ProductFeaturedSection />
     </>
   );
 }
