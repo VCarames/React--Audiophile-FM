9242e2fc263f405c5acb1924418d3afbdf785c1b
VCarames <109115310+VCarames@users.noreply.github.com>
2024-09-14 21:04:02 -0700

    AboutSection: Added Custom Class Prop
    In the updated code, a new prop "aboutSectionClass" has been introduced to the "AboutSection" component.

--- src/components/about/AboutSection.jsx
+++ src/components/about/AboutSection.jsx
@@ -1,9 +1,9 @@
 import AboutImages from "./AboutImages";
 import AboutText from "./AboutText";
 
-function AboutSection() {
+function AboutSection({ aboutSectionClass }) {
   return (
-    <section className="about">
+    <section className={`about ${aboutSectionClass}`}>
       <h2 className="visually-hidden">About Us</h2>
       <div className="about__content">
         <AboutImages />
