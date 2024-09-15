042d3d1b56658673cd520bb9ea8b86592b9a91bf
VCarames <109115310+VCarames@users.noreply.github.com>
2024-09-14 17:40:02 -0700

    CategoryLinkSection: Added Custom Class Prop
    Updated the "CategoryLinkSection" component to accept an additional "categoryLinkClass" prop for custom styling.

--- src/components/categoryLinks/CategoryLinkSection.jsx
+++ src/components/categoryLinks/CategoryLinkSection.jsx
@@ -1,8 +1,8 @@
 import CategorylinkList from "./CategoryLinkList";
 
-function CategoryLinkSection() {
+function CategoryLinkSection({ categoryLinkClass }) {
   return (
-    <section className="category">
+    <section className={`category ${categoryLinkClass}`}>
       <h2 className="visually-hidden">Product Categories</h2>
       <div className="category__content">
         <CategorylinkList />
