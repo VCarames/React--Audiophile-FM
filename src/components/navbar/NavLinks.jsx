11dddf1144af8c8a8a832e679a537ff580ef8c8d
VCarames <109115310+VCarames@users.noreply.github.com>
2024-09-15 09:52:10 -0700

    NavLinks: Moved Image Outside of Link
    In the updated "NavLinks" component, the image element has been moved outside the "Link" component for styling purposes.

--- src/components/navbar/NavLinks.jsx
+++ src/components/navbar/NavLinks.jsx
@@ -4,8 +4,8 @@ import Arrow from "/assets/shared/desktop/icon-arrow-right.svg";
 function NavLinks({ imgClass, img, to, category }) {
   return (
     <li className="nav__menu-item" role="listitem">
+      <img className={`nav__menu-img ${imgClass}`} src={img} alt="" />
       <Link className="nav__menu-link" to={to}>
-        <img className={`nav__menu-img ${imgClass}`} src={img} alt="" />
         <div className="nav__menu-group">
           <span className="nav__menu-category">{category}</span>
           <div className="nav__menu-group--shop">
