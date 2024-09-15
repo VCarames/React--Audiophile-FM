4f404b64bc96097f8ba60968d4931496e7e787f9
VCarames <109115310+VCarames@users.noreply.github.com>
2024-09-14 21:08:59 -0700

    ProductDetails: Added Category Link Section
    In the updated "ProductDetails" component, the "CategoryLinkSection" has been added.

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -1,12 +1,14 @@
 import ProductDetailsSection from "../productDetails/ProductDetailsSection";
 import ProductRecommendationSection from "../productRecommendations/ProductRecommendationSection";
+import CategoryLinkSection from "../components/categoryLinks/CategoryLinkSection";
 
 function ProductDetails() {
   return (
-    <main>
+    <>
       <ProductDetailsSection />
       <ProductRecommendationSection />
-    </main>
+      <CategoryLinkSection />
+    </>
   );
 }
 
