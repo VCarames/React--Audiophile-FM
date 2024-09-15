0f27ca0016260ec0e2aa1516b370a2fd30fbb6d4
VCarames <109115310+VCarames@users.noreply.github.com>
2024-09-15 10:05:35 -0700

    ProductFeaturedItem SCSS: Styled

--- src/utilities/formatCurrency.js
+++ src/utilities/formatCurrency.js
@@ -2,5 +2,7 @@ export function formatCurrency(value) {
   return new Intl.NumberFormat("en-US", {
     style: "currency",
     currency: "USD",
+    minimumFractionDigits: 0,
+    maximumFractionDigits: 0,
   }).format(value);
 }
