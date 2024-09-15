8dade348f16fda1b6c439b016551eb66808a4553
VCarames <109115310+VCarames@users.noreply.github.com>
2024-09-14 20:52:44 -0700

    Home Page: Added AboutSection
    The updated code introduces the "AboutSection" component to the "Home" page,.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import Hero from "../components/intros/Hero";
 import CategoryLinkSection from "../components/categoryLinks/CategoryLinkSection";
 import ProductFeaturedSection from "../productFeatured/ProductFeaturedSection";
+import AboutSection from "../components/about/AboutSection";
 
 function Home() {
   return (
@@ -8,6 +9,7 @@ function Home() {
       <Hero />
       <CategoryLinkSection categoryLinkClass="category--home" />
       <ProductFeaturedSection />
+      <AboutSection aboutSectionClass="about--home" />
     </>
   );
 }
