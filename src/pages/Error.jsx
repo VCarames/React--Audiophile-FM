12b63169ee2d63adb6f2ab563a4f2ed438c57a2b
VCarames <109115310+VCarames@users.noreply.github.com>
2024-09-15 09:45:05 -0700

    Error Page: Enhanced Page with Detailed Feedback and Navigation
    In the updated "Error" component, functionality has been expanded to improve user experience and error handling. The "useRouteError" hook is utilized to capture and display specific error messages dynamically, providing more context about what went wrong.

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,8 +1,33 @@
+import { Link, useRouteError } from "react-router-dom";
+import Logo from "/assets/shared/desktop/logo.svg";
+
 function Error() {
+  const error = useRouteError();
+
   return (
-    <div>
-      <h1>Error Page</h1>
-    </div>
+    <section className="error">
+      <div className="error__content">
+        <Link
+          className="error__logo-link button"
+          to="/"
+          aria-label="Audiophile - Home"
+        >
+          <img
+            className="error__logo-link--img"
+            src={Logo}
+            alt="Audiophile Logo"
+          />
+        </Link>
+        <h1 className="error__title">
+          Whoops...!! <span>Something went wrong 😢</span>
+        </h1>
+        <p className="error__message">{error.data || error.message}</p>
+
+        <Link className="button button--one" to="/">
+          Return Home
+        </Link>
+      </div>
+    </section>
   );
 }
 
