7048a22e473f48773e6c92bc1de7eac8324a3602
VCarames <109115310+VCarames@users.noreply.github.com>
2024-09-14 17:38:13 -0700

    CategorylinkList: Updated with Imported Images and Custom Classes
    Updated the "CategorylinkList" component to import images directly and pass them along with specific custom classes to the "CategoryLink" component.

--- src/components/categoryLinks/CategoryLinkList.jsx
+++ src/components/categoryLinks/CategoryLinkList.jsx
@@ -1,20 +1,26 @@
 import CategoryLink from "./CategoryLink";
+import HeadphonesImg from "/assets/shared/desktop/image-category-thumbnail-headphones.png";
+import SpeakerImg from "/assets/shared/desktop/image-category-thumbnail-speakers.png";
+import EarphonesImg from "/assets/shared/desktop/image-category-thumbnail-earphones.png";
 
 function CategorylinkList() {
   return (
     <ul className="category__list">
       <CategoryLink
-        img="public/assets/shared/desktop/image-category-thumbnail-headphones.png"
+        CategoryImageClass="category-link__image--headphones"
+        img={HeadphonesImg}
         heading="Headphones"
         link="/category/headphones"
       />
       <CategoryLink
-        img="public/assets/shared/desktop/image-category-thumbnail-speakers.png"
+        CategoryImageClass="category-link__image--speakers"
+        img={SpeakerImg}
         heading="Speakers"
         link="/category/speakers"
       />
       <CategoryLink
-        img="public/assets/shared/desktop/image-category-thumbnail-earphones.png"
+        CategoryImageClass="category-link__image--earphones"
+        img={EarphonesImg}
         heading="Earphones"
         link="/category/earphones"
       />
