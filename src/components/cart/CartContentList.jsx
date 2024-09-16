9db1b92e1a5c1efb3159ee746fa6711ba1765608
VCarames <109115310+VCarames@users.noreply.github.com>
2024-09-15 17:43:05 -0700

    CartContentList: Displays List of Cart Items
    The "CartContentList" component renders a list of cart items or a message indicating that the cart is empty. If there are items in the cart, it maps over them to render "CartItem" components for each item. If the cart is empty, it displays a message saying "Your cart is empty."

---
+++ src/components/cart/CartContentList.jsx
@@ -0,0 +1,19 @@
+import CartItem from "./CartItem";
+
+function CartContentList({ cart }) {
+  return cart.length === 0 ? (
+    <div className="cart__empty-container">
+      <p className="cart__empty-container--message">Your cart is empty.</p>
+    </div>
+  ) : (
+    <div className="cart__list--content">
+      <ul className="cart__list">
+        {cart.map((item) => (
+          <CartItem item={item} key={item.productId} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default CartContentList;
