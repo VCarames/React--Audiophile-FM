5a1e16c4f2bc0b48dacbcecc176cd8649f07fd2f
VCarames <109115310+VCarames@users.noreply.github.com>
2024-09-15 17:41:38 -0700

    CartContentItem: Displays Item Details in Cart
    The "CartContentItem" component displays details for an individual item in the cart, including its thumbnail, name, and price, which is formatted using "formatCurrency". It uses "useSelector" to get the current quantity of the item from the Redux store. The component also includes an "UpdateItemQuantity" button for modifying the item's quantity.

---
+++ src/components/cart/CartItem.jsx
@@ -0,0 +1,30 @@
+import { useSelector } from "react-redux";
+import { getCurrentQuantityById } from "/src/components/cart/CartSlice.js";
+import { formatCurrency } from "/src/utilities/formatCurrency.js";
+import UpdateItemQuantity from "../buttons/UpdateItemQuantity";
+
+function CartContentItem({ item }) {
+  const { id, thumbnail, name, currentPrice, totalPrice } = item;
+  const currentQuantity = useSelector(getCurrentQuantityById(id));
+
+  return (
+    <li className="cart-content-item" role="listitem">
+      <img
+        className="cart-content-item__thumbnail"
+        src={thumbnail}
+        alt={name}
+      />
+
+      <div className="cart-content-item__info">
+        <p className="cart-content-item__name">{name}</p>
+        <p className="cart-content-item__price">
+          {formatCurrency(currentPrice)}
+        </p>
+      </div>
+
+      <UpdateItemQuantity productId={id} currentQuantity={currentQuantity} />
+    </li>
+  );
+}
+
+export default CartContentItem;
