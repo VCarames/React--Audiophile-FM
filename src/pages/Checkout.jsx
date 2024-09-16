cf5cbd3cb852a09eb3dddc3643e7dd7a099c8f00
VCarames <109115310+VCarames@users.noreply.github.com>
2024-09-16 16:40:52 -0700

    Checkout: Main Checkout Page Component
    The "Checkout" component serves as the main page for the checkout process. It includes navigation to go back to the previous page, customer and shipping information forms ("CheckoutCustomerInfo" and "CheckoutShippingInfo"), and a summary of the order ("CheckoutSummary").

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,7 +1,26 @@
+import { Link } from "react-router-dom";
+import CheckoutCustomerInfo from "../checkout/CheckoutCustomerInfo";
+import CheckoutShippingInfo from "../checkout/CheckoutShippingInfo";
+import CheckoutSummary from "../checkout/CheckoutSummary";
+
 function Checkout() {
   return (
-    <div>
-      <h1>Checkout Page</h1>
+    <div className="checkout">
+      <div className="checkout__content">
+        <Link to="/" className="checkout__back-link button">
+          Go Back
+        </Link>
+        <div className="checkout__details">
+          <div className="checkout__group">
+            <h1 className="checkout__title">Checkout</h1>
+            <div className="checkout__forms">
+              <CheckoutCustomerInfo />
+              <CheckoutShippingInfo />
+            </div>
+          </div>
+          <CheckoutSummary />
+        </div>
+      </div>
     </div>
   );
 }
