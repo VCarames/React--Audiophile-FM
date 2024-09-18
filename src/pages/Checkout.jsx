2507984ac3d285b324afd24e0bc454e9fd9285ec
VCarames <109115310+VCarames@users.noreply.github.com>
2024-09-17 20:54:55 -0700

    Checkout: Added Validation for Email and Phone Number
    The updated "Checkout" component introduces validation for email and phone number fields using regular expressions. It checks for a valid email format and ensures phone numbers are exactly 10 digits.

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -29,24 +29,37 @@ function Checkout() {
     e.preventDefault();
     let validationErrors = {};
 
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const phoneRegex = /^[0-9]{10}$/;
+
     if (formValues.name.trim() === "") {
       validationErrors.name = "Name cannot be empty!";
     }
+
     if (formValues.email.trim() === "") {
       validationErrors.email = "Email cannot be empty!";
+    } else if (!emailRegex.test(formValues.email)) {
+      validationErrors.email = "Enter a valid email!";
     }
+
     if (formValues.phone.trim() === "") {
       validationErrors.phone = "Phone cannot be empty!";
+    } else if (!phoneRegex.test(formValues.phone)) {
+      validationErrors.phone = "Enter a valid phone number!";
     }
+
     if (formValues.address.trim() === "") {
       validationErrors.address = "Address cannot be empty!";
     }
+
     if (formValues.city.trim() === "") {
       validationErrors.city = "City cannot be empty!";
     }
+
     if (formValues.zip.trim() === "") {
       validationErrors.zip = "Zip cannot be empty!";
     }
+
     if (formValues.country.trim() === "") {
       validationErrors.country = "Country cannot be empty!";
     }
