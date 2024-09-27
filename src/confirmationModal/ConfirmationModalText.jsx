a97c685ffbeb3e60510323895d0e7d05d901401b
VCarames <109115310+VCarames@users.noreply.github.com>
2024-09-26 19:56:07 -0700

    ConfirmationModalText: Accessibility Improvement
    In the updated "ConfirmationModalText" component, an id has been added to the <h2> element to enhance accessibility.

--- src/confirmationModal/ConfirmationModalText.jsx
+++ src/confirmationModal/ConfirmationModalText.jsx
@@ -6,7 +6,9 @@ function ConfirmationModalText() {
         src="/assets/checkout/icon-order-confirmation.svg"
         alt="Order Confirmation"
       />
-      <h2 className="confirmation-modal__title">Thank you for your order</h2>
+      <h2 className="confirmation-modal__title" id="confirmation-modal__title">
+        Thank you for your order
+      </h2>
       <p className="confirmation-modal__message">
         You will receive an email confirmation shortly.
       </p>
